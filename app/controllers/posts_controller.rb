4520500685c46d553d0fdd1568c23aa18889a30f
Vieenay Siingh <vieenaysiingh@gmail.com>
2016-10-05 01:44:20 +0800

    added post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,24 @@
 class PostsController < ApplicationController
 	def index
+		@posts = Post.all
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def create
+		#@post = Post.create(posts_params)
+		@post = Post.new(posts_params)
+		if @post.save
+			redirect_to @post
+	  else
+	   render "new"	
+	  end	
 	end
 
 	def show
+		@post = Post.find(params[:id])
 	end
 
 	def edit
@@ -21,7 +31,9 @@ class PostsController < ApplicationController
 	end
 
 	private 
+
 	def posts_params
+		params.require(:post).permit(:image, :caption)
 	end
 	
 end
